f2f59a0b0a194948efd8fd7d8d92db68cc77996d
Tallys Augusto Goncalves Cordeiro <tallys.cordeiro@unb.br>
2023-06-14 11:28:21 -0300

    slide pronto

--- src/app/componentes/home/mouse-slide/mouse-slide.component.ts
+++ src/app/componentes/home/mouse-slide/mouse-slide.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mouse-slide',
   templateUrl: './mouse-slide.component.html',
   styleUrls: ['./mouse-slide.component.css']
 })
-export class MouseSlideComponent {
+export class MouseSlideComponent implements OnInit{
+  cards : HTMLDivElement[] = [];
+  cards_desc : HTMLDivElement[] = [];
 
+  onHoverIn(num: number ){
+    this.cards.forEach((element,i) => {
+      if(i != num){
+        element.classList.remove('apres');
+        this.cards_desc = Array.from(element.querySelectorAll('.description')) as HTMLDivElement[];
+        this.cards_desc[0].style.opacity = '0';
+      }else{
+        element.classList.add('apres');
+        this.cards_desc = Array.from(element.querySelectorAll('.description')) as HTMLDivElement[];
+        this.cards_desc[0].style.opacity = '1';
+      }
+
+    });
+
+  }
+
+  ngOnInit(): void {
+    this.cards = Array.from(document.querySelectorAll('.card')) as HTMLDivElement[];
+    this.cards_desc = Array.from(this.cards[0].querySelectorAll('.description')) as HTMLDivElement[];
+    this.cards_desc[0].style.opacity = '1';
+    this.cards[0].classList.add('apres');
+  }
 }
